1a87d4737fd6cef70ef601d43f45aea64c167f30
Всеволод Шалдин <shaldin2000@mail.ru>
2022-09-06 19:28:27 +0300

    Added CORS in .env config

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import "dotenv/config";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: [
-          'http://localhost:3000',
-          'http://localhost:8080',
-        ],
+        origin: process.env.CORS.split(','),
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
       });
