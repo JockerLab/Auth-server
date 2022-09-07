54a1648e8482adff6d4d482a05e92413037ff833
Всеволод Шалдин <shaldin2000@mail.ru>
2022-09-07 20:00:45 +0300

    Added doc to controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,17 +7,31 @@ import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 export class AppController {
     constructor(private readonly authService: AuthService) {}
 
+    /**
+     * Method that register a user with given credentials.
+     * @param registerUserDto {@link RegisterUserDto} user credentials to register.
+     * @returns jwt token if user registered successfully, else an error.
+     */
     @Post('register')
     async register(@Body() registerUserDto: RegisterUserDto) {
         return this.authService.register(registerUserDto);
     }
 
+    /**
+     * Method that log-in a user with given credentials.
+     * @param req user credentials to log-in.
+     * @returns jwt token if user logged-in successfully, else an error.
+     */
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user);
     }
 
+    /**
+     * Method that log-out a user.
+     * @returns {null}.
+     */
     @Post('logout')
     async logout() {
         return null;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,24 +8,46 @@ import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
+    /**
+     * Check if user have jwt token and create user info with given data.
+     * @param createInfoDto given data.
+     * @param req user creadentials.
+     * @returns user with created info or an error.
+     */
     @UseGuards(JwtAuthGuard)
     @Post()
     async createInfo(@Body() createInfoDto: CreateInfoDto, @Request() req) {
         return this.usersService.createInfo(req.user, createInfoDto);
     }
 
+    /**
+     * Check if user have jwt token and get user info.
+     * @param req user creadentials.
+     * @returns user with info or an error.
+     */
     @UseGuards(JwtAuthGuard)
     @Get()
     async getInfo(@Request() req) {
         return this.usersService.getInfo(req.user);
     }
 
+    /**
+     * Check if user have jwt token and edit user info with given data.
+     * @param editInfoDto {@link EditInfoDto} given data.
+     * @param req user creadentials.
+     * @returns user with edited info or an error.
+     */
     @UseGuards(JwtAuthGuard)
     @Put()
     async editInfo(@Body() editInfoDto: EditInfoDto, @Request() req) {
         return this.usersService.editInfo(req.user, editInfoDto);
     }
 
+    /**
+     * Check if user have jwt token and delete user info.
+     * @param req user creadentials.
+     * @returns user without info or an error.
+     */
     @UseGuards(JwtAuthGuard)
     @Delete()
     async deleteInfo(@Request() req) {
